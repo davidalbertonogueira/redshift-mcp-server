ce56f5536e4f8b1c19b1afeaa7d8ede655e386f8
David Nogueira <dnogueira@rauva.com>
2025-10-15 17:47:20 +0100

    remove legacy files

--- inspect-schema.ts
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-import { ListToolsRequestSchema } from "@modelcontextprotocol/sdk/types.js";
-
-console.log("ListToolsRequestSchema:", JSON.stringify(ListToolsRequestSchema, null, 2));
